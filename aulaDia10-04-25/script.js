7327218f338208ee7366b530b78e7c7c2810e31c
Augusto <augustocesarrezende64@gmail.com>
2025-04-10 19:38:16 -0300

    atualizacao

--- aulaDia10-04-25/script.js
+++ aulaDia10-04-25/script.js
@@ -1,6 +1,8 @@
-const user = {
-    nome : "augusto",
-    idade : 28
-};
-
-console.log(user.nome);
+window.addEventListener("DOMContentLoaded", () => {
+    const getDataWithFetch = async () => {
+        const data = await fetch("https://augusto-debug.github.io/PROJETO-FRONT-END-WEB-JAVASCRIPT/aulaDia10-04-25/");
+        console.log(data);
+        
+    }
+    getDataWithFetch();
+});
